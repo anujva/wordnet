4dd737f9f495d886b0e29c7325091b227191c77f
Anuj Varma <anujva@gmail.com>
2018-04-21 11:34:03 -0700

    some changes to wordnet

--- src/main/java/com/coursera/WordNet.java
+++ src/main/java/com/coursera/WordNet.java
@@ -2,11 +2,16 @@ package com.coursera;
 
 import edu.princeton.cs.algs4.Digraph;
 
-import java.util.Objects;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.function.Consumer;
 
-public class WordNet {
+public class WordNet implements Iterable<String> {
     private Digraph digraph;
+    private List<WordNetNode> wordNetNodes;
+    private Set<String> nouns;
 
     /**
      * The constructor takes in the name of the input files
@@ -14,34 +19,82 @@ public class WordNet {
      * @param synsets
      * @param hypernyms
      */
-    public WordNet(String synsets, String hypernyms) {
+    public WordNet(String synsets, String hypernyms) throws IOException {
+        /*
+        * The first thing to do would be to read in the synsets file,
+        * and create the digraph nodes
+         */
 
+        BufferedReader reader = new BufferedReader(new FileReader(synsets));
+        String line;
+        wordNetNodes = new ArrayList<>();
+        nouns = new HashSet<>();
+        while ((line = reader.readLine()) != null) {
+            String[] splitLine = line.split(",");
+            wordNetNodes.add(new WordNetNode(Integer.parseInt(splitLine[0]), splitLine[1], splitLine[2]));
+            Arrays.stream(splitLine[1].split(" ")).forEach(x -> nouns.add(x));
+        }
+
+        digraph = new Digraph(wordNetNodes.size());
+        reader.close();
+        reader = new BufferedReader(new FileReader(hypernyms));
+
+        while ((line = reader.readLine()) != null) {
+            String[] splitLine = line.split(",");
+            for (int i = 1; i < splitLine.length; i++) {
+                digraph.addEdge(Integer.parseInt(splitLine[0]), Integer.parseInt(splitLine[i]));
+            }
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        WordNet wordNet = new WordNet("C:\\Users\\anujv\\OneDrive\\Desktop\\wordnet-testing\\wordnet\\synsets.txt",
+                "C:\\Users\\anujv\\OneDrive\\Desktop\\wordnet-testing\\wordnet\\hypernyms.txt");
 
+        for (String noun : wordNet) {
+            System.out.println(noun);
+        }
     }
 
     public Iterable<String> nouns() {
-
+        return this;
     }
 
     public boolean isNoun(String word) {
-
+        throw new UnsupportedOperationException();
     }
 
     public int distance(String nounA, String nounB) {
-
+        throw new UnsupportedOperationException();
     }
 
     public String sap(String nounA, String nounB) {
+        return null;
+    }
 
+    @Override
+    public Iterator<String> iterator() {
+        return new WordNetIterator(this);
+    }
+
+    @Override
+    public void forEach(Consumer<? super String> action) {
+        for (String noun : nouns) {
+            action.accept(noun);
+        }
     }
 
     private static final class WordNetNode {
         private int synsetId;
         private Set<String> synset;
         private String gloss;
+
+        public WordNetNode(int synsetId, String synset, String gloss) {
+            this.synsetId = synsetId;
+            String[] splitSynset = synset.split(" ");
+            this.synset = new HashSet<>(Arrays.asList(splitSynset));
+            this.gloss = gloss;
+        }
 
         public WordNetNode(int synsetId, Set<String> synset, String gloss) {
             this.synsetId = synsetId;
@@ -62,6 +115,25 @@ public class WordNet {
         @Override
         public int hashCode() {
             return Objects.hash(synsetId, synset, gloss);
+        }
+    }
+
+    public static class WordNetIterator implements Iterator<String> {
+        private Iterator<String> iterator;
+
+        public WordNetIterator(WordNet wordNet) {
+            iterator = wordNet.nouns.iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public String next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return iterator.next();
         }
     }
 }
