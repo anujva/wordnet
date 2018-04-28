1832692b04368fa0ee836e7203863326b2db3586
Anuj Varma <anujva@gmail.com>
2018-04-28 13:26:45 -0700

    SAP tested for a few cases, and README expanded

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # wordnet
 COS 226 Programming assignment 1
+
+The first class to be implemented needs to be SAP. This implementation of SAP has an api for finding the Shortest Common Ancestor (SCA) and in turn,
+finding the Shortest Ancestral Path (SAP).
--- src/main/java/com/coursera/SAP.java
+++ src/main/java/com/coursera/SAP.java
@@ -24,16 +24,20 @@ public class SAP {
         List<Integer> queueForW = new LinkedList<>();
 
         int length = Integer.MAX_VALUE;//the farthest possible away
+        queueForV.add(v);
+        queueForW.add(w);
+        vNeighbours.put(v, 0);
+        wNeighbours.put(w, 0);
         while (!queueForV.isEmpty() || !queueForW.isEmpty()) {
-            Integer vInt = queueForV.isEmpty() ? ((LinkedList<Integer>) queueForV).getFirst() : null;
+            Integer vInt = !queueForV.isEmpty() ? ((LinkedList<Integer>) queueForV).poll() : null;
             //find the connections for the particular integer..
             //if there is a connection there..
-            Integer wInt = queueForW.isEmpty() ? ((LinkedList<Integer>) queueForW).getFirst() : null;
+            Integer wInt = !queueForW.isEmpty() ? ((LinkedList<Integer>) queueForW).poll() : null;
             if (vInt != null && wNeighbours.containsKey(vInt)) {
                 //This would mean that we have seen this node on the bfs that was started from the
                 //node w.
                 ancestors.put(vInt, wNeighbours.get(vInt) + vNeighbours.get(vInt));
-                if (length < wNeighbours.get(vInt) + vNeighbours.get(vInt)) {
+                if (length > wNeighbours.get(vInt) + vNeighbours.get(vInt)) {
                     length = wNeighbours.get(vInt) + vNeighbours.get(vInt);
                 }
             }
@@ -41,7 +45,7 @@ public class SAP {
             if (wInt != null && vNeighbours.containsKey(wInt)) {
                 //Same thing with w
                 ancestors.put(wInt, vNeighbours.get(wInt) + wNeighbours.get(wInt));
-                if (length < vNeighbours.get(wInt) + wNeighbours.get(wInt)) {
+                if (length > vNeighbours.get(wInt) + wNeighbours.get(wInt)) {
                     length = vNeighbours.get(wInt) + wNeighbours.get(wInt);
                 }
             }
@@ -65,5 +69,23 @@ public class SAP {
             }
         }
         return length;
+    }
+
+    public static void main(String[] args) {
+        Digraph digraph = new Digraph(13);
+        digraph.addEdge(1, 0);
+        digraph.addEdge(2, 0);
+        digraph.addEdge(3, 1);
+        digraph.addEdge(4, 1);
+        digraph.addEdge(5, 1);
+        digraph.addEdge(7, 3);
+        digraph.addEdge(8, 3);
+        digraph.addEdge(9, 5);
+        digraph.addEdge(10, 5);
+        digraph.addEdge(11, 10);
+        digraph.addEdge(12, 10);
+
+        SAP sap = new SAP(digraph);
+        System.out.println(sap.length(3, 11));
     }
 }
